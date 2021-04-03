b6136114877b2cfad616f3ac82d956d70aa11bce
SCYeon <50772004+SCYeon@users.noreply.github.com>
2021-04-03 21:40:29 +0900

    [출력 및 사칙연산]

--- 0403.py
+++ 0403.py
@@ -1,1 +1,58 @@
 print("Connect First")
+
+# 입출력과 사칙연산
+# 2557
+print("Hello World!")
+
+# 10718
+print("강한친구 대한육군");
+print("강한친구 대한육군");
+
+# 10171
+print("\\    /\\\n )  ( ')\n(  /  )\n \(__)|")
+
+# 10172
+print('|\\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\\__|')
+
+# 1000
+a,b = input().split()
+a=int(a)
+b=int(b)
+print(a+b)
+
+# 1001
+print(a-b)
+
+# 10998
+print(a*b)
+
+# 1008
+print(a/b)
+
+# 10869
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+# 10430
+x,y,z = input().split()
+x=int(x)
+y=int(y)
+z=int(z)
+print((x+y)%z)
+print(((x%z)+(y%z))%z)
+print((x*y)%z)
+print(((x%z)*(y%z))%z)
+
+# 2588
+i=int(input())
+j=input()
+
+k1 = i*int(j[2])
+k2 = i*int(j[1])
+k3 = i*int(j[0])
+k4 = i*int(j)
+
+print(k1, k2, k3, k4, sep='\n')
